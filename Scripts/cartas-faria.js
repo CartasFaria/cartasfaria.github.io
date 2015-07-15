0faad510bca33fc0b8aa3fab53f8908b2a80f38a
unknown <placerda@cit011261.cit>
2015-07-15 09:21:38 -0300

    Inclusão do menu

--- Scripts/cartas-faria.js
+++ Scripts/cartas-faria.js
@@ -5,7 +5,19 @@
         var service = {};
 
         service.getCards = function () {
-            return $http.get("http://crossorigin.me/http://docs.google.com/spreadsheets/d/1T7MpDLrNndOFKDnzEZvG0tFDsphZx6BW7Qg-o4xmr_o/pub?gid=0&single=true&output=tsv");
+            return $http.get("//crossorigin.me/https://docs.google.com/spreadsheets/d/1T7MpDLrNndOFKDnzEZvG0tFDsphZx6BW7Qg-o4xmr_o/pub?gid=0&single=true&output=tsv");
+        };
+
+        service.queryStringToJSON = function () {
+            var pairs = location.search.slice(1).split('&');
+
+            var result = {};
+            pairs.forEach(function (pair) {
+                pair = pair.split('=');
+                result[pair[0]] = decodeURIComponent(pair[1] || '');
+            });
+
+            return JSON.parse(JSON.stringify(result));
         };
 
         return service;
@@ -18,7 +30,20 @@
 
         this.cards = [];
         this.selectedColor = '';
+        this.filter = service.queryStringToJSON();
+        
+        this.filterCards = function (card) {
 
+            if ($this.filter.cardID) {
+                return card.id == $this.filter.cardID;
+            }
+
+            if ($this.filter.color) {
+                return card.color.indexOf($this.filter.color) >= 0;
+            }
+
+            return true;
+        }
 
         // Carrega as cartas
         service.getCards()
